b59ed540a0d75454f7d2f41279307e5dd65e79cb
Dwayne Hurst <hurstyd@gmail.com>
2019-05-08 10:47:05 +1000

    List updated 8 May 2019

--- hurstythemes.sh
+++ hurstythemes.sh
@@ -891,6 +891,7 @@ function 16x9_themes() {
         'RetroHursty69 boxalloyred'
         'RetroHursty69 boxcity'
         'RetroHursty69 cabsnazzy'
+	'RetroHursty69 CapcomColorHorizontal'
         'RetroHursty69 cardcrazy'
         'RetroHursty69 circuit'
         'RetroHursty69 comiccrazy'
