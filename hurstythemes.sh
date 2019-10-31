0bff397fd26f12875793e32f3aa050b57827f899
Dwayne Hurst <hurstyd@gmail.com>
2019-10-31 21:04:15 +1000

    Updated 31 Oct 2019

--- hurstythemes.sh
+++ hurstythemes.sh
@@ -87,7 +87,7 @@ function gui_hurstythemes() {
 		options+=(Q "Slick Themes Manager (29 Themes)")
 		options+=(R "Hyper Themes Manager (177 Themes)")
 		options+=(S "Mario Themes Manager (22 Themes)")
-		options+=(T "GPi (320x240) Themes Manager (14 Themes)")				
+		options+=(T "GPi (320x240) Themes Manager (15 Themes)")				
 
         local i=1
         for theme in "${themes[@]}"; do
@@ -104,7 +104,7 @@ function gui_hurstythemes() {
             fi
             ((i++))
         done
-        local cmd=(dialog --default-item "$default" --backtitle "Hursty's ES Themes Installer" --menu "Hursty's ES Themes Installer - (1257 Themes as at 30 October 2019)" 22 76 16)
+        local cmd=(dialog --default-item "$default" --backtitle "Hursty's ES Themes Installer" --menu "Hursty's ES Themes Installer - (1258 Themes as at 31 October 2019)" 22 76 16)
         local choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
         default="$choice"
         [[ -z "$choice" ]] && break
@@ -2284,6 +2284,7 @@ function GPi_themes() {
         'RetroHursty69 GPi_HeyChromeyOfficial'
         'RetroHursty69 GPi_PopBox'
         'RetroHursty69 GPi_Retroroid'
+        'RetroHursty69 GPi_SteelChromey'
 		'RetroHursty69 GPi_SuperSweet'
         'RetroHursty69 GPi_Sublime'
 		'RetroHursty69 GPi_Soda'
