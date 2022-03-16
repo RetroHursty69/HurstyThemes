54e5c5ebe45461a96065fb8275af2e2c0a8f4f1c
RetroHursty69 <hurstyd@gmail.com>
2022-03-17 04:21:26 +1000

    Updated

--- hurstythemes.sh
+++ hurstythemes.sh
@@ -76,7 +76,7 @@ function gui_hurstythemes() {
         options+=(F "Mini Sweet Themes Manager (240 Themes)")
         options+=(G "Cool Themes Manager (121 Themes)")
         options+=(H "Spin Themes Manager (172 Themes)")
-        options+=(I "16:9 Aspect Themes Manager (100 Themes)")
+        options+=(I "16:9 Aspect Themes Manager (101 Themes)")
         options+=(J "5:4 Aspect Themes Manager (7 Themes)")
         options+=(K "Vertical Aspect Themes Manager (2 Themes)")
         options+=(L "Chromey Blue Themes Manager (133 Themes)")
@@ -105,7 +105,7 @@ function gui_hurstythemes() {
             fi
             ((i++))
         done
-        local cmd=(dialog --default-item "$default" --backtitle "Hursty's ES Themes Installer" --menu "Hursty's ES Themes Installer - (Well over 1300 Themes as at 21 January 2022)" 22 76 16)
+        local cmd=(dialog --default-item "$default" --backtitle "Hursty's ES Themes Installer" --menu "Hursty's ES Themes Installer - (Well over 1300 Themes as at 15 March 2022)" 22 76 16)
         local choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
         default="$choice"
         [[ -z "$choice" ]] && break
@@ -1005,6 +1005,7 @@ function 16x9_themes() {
         'RetroHursty69 tributeSTrek'
         'RetroHursty69 tributeSWars'
 		'RetroHursty69 uniflyered'
+		'RetroHursty69 Vinyl-Hits'
         'RetroHursty69 whiteslide'
         'RetroHursty69 whitewood'
     )
